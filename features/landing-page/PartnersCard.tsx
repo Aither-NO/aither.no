d4f2d90f45e720580d0bd719ab9e5c690b0eb8c1
Titas Virbickas <147817617+titusvi@users.noreply.github.com>
2025-01-11 15:02:49 +0200

    feat: add EDIH and InnovasjonNorge to partners

--- features/landing-page/PartnersCard.tsx
+++ features/landing-page/PartnersCard.tsx
@@ -23,14 +23,24 @@ export function PartnersCard() {
         wrap="wrap"
       >
         <PartnerImgLink
-          href="https://www.lindesnes.kommune.no"
-          alt="Lindesnes Kommune"
-          src="https://cdn.sanity.io/images/2vqjviqb/content/990f16a54c63777d807b341bd94eaf5b4e24b437-3426x1910.jpg?w=300"
+          href="https://european-digital-innovation-hubs.ec.europa.eu/home"
+          alt="EDIH - European Digital Innovation Hubs Network"
+          src="https://i.postimg.cc/YSVbF18x/EDIH-logo.png"
         />
         <PartnerImgLink
           href="https://www.norceresearch.no"
           alt="Norce Research"
           src="https://cdn.sanity.io/images/2vqjviqb/content/edfe001e7e4999489064e7194ece14e66c19b43c-911x265.jpg?w=300"
+        />
+        <PartnerImgLink
+          href="https://www.innovasjonnorge.no/"
+          alt="Innovasjon Norge"
+          src="https://i.postimg.cc/dtmHfZtq/IN-logo.png"
+        />
+        <PartnerImgLink
+          href="https://www.lindesnes.kommune.no"
+          alt="Lindesnes Kommune"
+          src="https://cdn.sanity.io/images/2vqjviqb/content/990f16a54c63777d807b341bd94eaf5b4e24b437-3426x1910.jpg?w=300"
         />
       </Flex>
     </Card>
